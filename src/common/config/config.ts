58daa847ba17d9413205c0461e197697f7ef590c
kimsun721 <kimsun050820@gmail.com>
2026-03-23 10:28:32 +0900

    chore: add docker-compose

--- src/common/config/config.ts
+++ src/common/config/config.ts
@@ -2,4 +2,8 @@ export default () => ({
   SERVER_PORT: process.env.SERVER_PORT,
   DATABASE_URL: process.env.DATABASE_URL,
   FRONT_URL: process.env.FRONT_URL,
+  REDIS_HOST: process.env.REDIS_HOST,
+  REDIS_PORT: process.env.REDIS_PORT,
+  REDIS_PASSWORD: process.env.REDIS_PASSWORD,
+  JWT_SECRET: process.env.JWT_SECRET,
 });
